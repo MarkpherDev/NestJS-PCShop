c3663c9c9823bec9f2aadef76ba21920e4f17594
martin <MartinChrisPC@hotmail.com>
2024-02-08 18:59:55 -0500

    ADD config multer

--- src/common/contants.ts
+++ src/common/contants.ts
@@ -1,1 +1,14 @@
-export const defaultImage = 'default.jpg'
+import { diskStorage } from 'multer'
+
+export const defaultImage = './default.jpg'
+
+export const storage = diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './images')
+  },
+  filename: (req, file, cb) => {
+    const fileExt = file?.mimetype?.split('/')[1]
+    const fileGen = `${Date.now()}.${fileExt}`
+    cb(null, fileGen)
+  }
+})
