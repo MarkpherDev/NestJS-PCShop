7e3555987381d5c61aafbf18a3c657d9063fef52
martin <MartinChrisPC@hotmail.com>
2024-02-09 22:13:20 -0500

    FIXED logger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { Logger, ValidationPipe } from '@nestjs/common'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -12,8 +12,7 @@ async function bootstrap() {
       transform: true
     })
   )
-  const logger = new Logger()
+
   await app.listen(3000)
-  logger.log(`Iniciando Api en http://localhost:3000/api/v1`)
 }
 bootstrap()
